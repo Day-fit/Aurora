ae17a489ec05ab85a733d7a03ae5450e7b5c207b
Day-fit <xdayfit@gmail.com>
2025-10-17 09:21:58 +0200

    Initialize aurora-core project with Spring Boot, PostgreSQL, Docker Compose, and Kotlin integration. Add Maven wrapper and IDE configuration files.

---
+++ aurora-core/src/main/kotlin/pl/dayfit/auroracore/AuroraCoreApplication.kt
@@ -0,0 +1,11 @@
+package pl.dayfit.auroracore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuroraCoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<AuroraCoreApplication>(*args)
+}
---
+++ aurora-core/src/test/kotlin/pl/dayfit/auroracore/AuroraCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.dayfit.auroracore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuroraCoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
