26fe0ab3d060db9c701835998ac1e942e8e11792
day-fit <xdayfit@gmail.com>
2025-11-10 11:39:32 +0100

    Enhance EducationDegree and SkillLevel enums: add labels for better readability and override toString method for improved string representation

--- aurora-core/src/main/kotlin/pl/dayfit/auroracore/type/EducationDegree.kt
+++ aurora-core/src/main/kotlin/pl/dayfit/auroracore/type/EducationDegree.kt
@@ -1,15 +1,22 @@
 package pl.dayfit.auroracore.type
 
-enum class EducationDegree {
-    LOWER_SECONDARY_SCHOOL,
-    UPPER_SECONDARY_SCHOOL,
-    VOCATIONAL_SCHOOL,
-    HIGH_SCHOOL,
-    ASSOCIATE_DEGREE,
-    BACHELOR_DEGREE,
-    MASTER_DEGREE,
-    DOCTORAL_DEGREE,
-    POSTDOCTORAL,
-    PROFESSIONAL_CERTIFICATE,
-    OTHER
+enum class EducationDegree(val label: String) {
+    LOWER_SECONDARY_SCHOOL("Lower secondary school"),
+    UPPER_SECONDARY_SCHOOL("Upper secondary school"),
+    VOCATIONAL_SCHOOL("Vocational school"),
+    HIGH_SCHOOL("High school"),
+    TECHNICAL_COLLEGE("Technical college"),
+    COLLEGE("College"),
+    UNIVERSITY("University"),
+    ASSOCIATE_DEGREE("Associate degree"),
+    BACHELOR_DEGREE("Bachelor degree"),
+    MASTER_DEGREE("Master degree"),
+    DOCTORAL_DEGREE("Doctoral degree"),
+    POSTDOCTORAL("Postdoctoral"),
+    PROFESSIONAL_CERTIFICATE("Professional certificate"),
+    OTHER("Other");
+
+    override fun toString(): String {
+        return label
+    }
 }
