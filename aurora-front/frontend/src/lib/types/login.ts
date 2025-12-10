9ba25aab93d29571dc13c980b704447e734a0ed3
jan <jan.zdan1234@gmail.com>
2025-12-10 21:39:41 +0100

    Erasing frontend dir

--- aurora-front/frontend/src/lib/types/login.ts
+++
@@ -1,8 +0,0 @@
-import {z} from "zod";
-
-export const LoginSchema = z.object({
-    identifier: z.string().min(1, "Identifier is required"),
-    password: z.string().min(1, "Password is required")
-});
-
-export type LoginValues = z.infer<typeof LoginSchema>;
