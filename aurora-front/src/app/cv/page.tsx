afabb931a9372a3d939ba429b20d809163a79025
jan <jan.zdan1234@gmail.com>
2025-12-15 11:01:51 +0100

    Upgrade dependencies and enhance CV page structure
    - Updated `next`, `react`, and various optional dependency versions in `package-lock.json` to latest patch (16.0.10).
    - Introduced structured layout for CV page with styled header and section components using Tailwind.

--- aurora-front/src/app/cv/page.tsx
+++ aurora-front/src/app/cv/page.tsx
@@ -1,3 +1,12 @@
 export default function Page() {
-  return <></>;
+
+    return (
+        <section className="rounded-xl p-10 md:p-16 min-h-[calc(100vh-80px)]">
+            <div className="bg-main-dark/80 backdrop-blur-sm rounded-xl p-8 md:p-12 flex flex-col md:flex-row gap-10 items-center text-text-dark shadow-2xl">
+                <header>
+
+                </header>
+            </div>
+        </section>
+    );
 }
