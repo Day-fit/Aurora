e0c1f301109b67b17987c6d9b80913f81e3ec46f
dayfit <xdayfit@gmail.com>
2026-01-14 18:12:23 +0100

    Update CORS headers and callback URLs
    - Allow all headers in CORS configuration (`allowedHeaders = *`)
    - Update `aurora.callback-url` in dev and production properties to use `/auth/callback` instead of `/oauth/callback`

--- aurora-auth-lib/src/main/kotlin/pl/dayfit/auroraauthlib/configuration/properties/CorsConfigurationProperties.kt
+++ aurora-auth-lib/src/main/kotlin/pl/dayfit/auroraauthlib/configuration/properties/CorsConfigurationProperties.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 class CorsConfigurationProperties {
     lateinit var allowedOrigins: List<String>
     var allowedMethods: List<String> = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
-    var allowedHeaders: List<String> = listOf("Authorization", "Content-Type")
+    var allowedHeaders: List<String> = listOf("*")
     var exposedHeaders: List<String> = listOf("Authorization")
     var allowCredentials: Boolean = true
     var maxAge: Long = 3600L
