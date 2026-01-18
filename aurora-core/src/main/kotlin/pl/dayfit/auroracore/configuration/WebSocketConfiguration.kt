f2a41a2af0d44520920be989e47ad80a30e71776
dayfit <xdayfit@gmail.com>
2026-01-18 20:23:24 +0100

    Add CORS support to WebSocket configuration and simplify token retrieval logic in `TrackerHandshakeInterceptor`.

--- aurora-core/src/main/kotlin/pl/dayfit/auroracore/configuration/WebSocketConfiguration.kt
+++ aurora-core/src/main/kotlin/pl/dayfit/auroracore/configuration/WebSocketConfiguration.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+import pl.dayfit.auroraauthlib.configuration.properties.CorsConfigurationProperties
 import pl.dayfit.auroracore.websocket.handler.TrackerStatusHandler
 import pl.dayfit.auroracore.websocket.interceptor.TrackerHandshakeInterceptor
 
@@ -11,10 +12,13 @@ import pl.dayfit.auroracore.websocket.interceptor.TrackerHandshakeInterceptor
 @EnableWebSocket
 class WebSocketConfiguration(
     private val trackerStatusHandler: TrackerStatusHandler,
-    private val trackerHandshakeInterceptor: TrackerHandshakeInterceptor
+    private val trackerHandshakeInterceptor: TrackerHandshakeInterceptor,
+    private val corsConfigurationProperties: CorsConfigurationProperties
 ) : WebSocketConfigurer {
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
         registry.addHandler(trackerStatusHandler, "/ws/tracker")
+            .setAllowedOrigins(*corsConfigurationProperties.allowedOrigins
+                .toTypedArray())
             .addInterceptors(trackerHandshakeInterceptor)
     }
 }
--- aurora-core/src/main/kotlin/pl/dayfit/auroracore/websocket/interceptor/TrackerHandshakeInterceptor.kt
+++ aurora-core/src/main/kotlin/pl/dayfit/auroracore/websocket/interceptor/TrackerHandshakeInterceptor.kt
@@ -1,17 +1,16 @@
 package pl.dayfit.auroracore.websocket.interceptor
 
-import org.springframework.http.HttpHeaders
 import org.springframework.http.server.ServerHttpRequest
 import org.springframework.http.server.ServerHttpResponse
-import org.springframework.http.server.ServletServerHttpRequest
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.WebSocketHandler
 import org.springframework.web.socket.server.HandshakeInterceptor
+import org.springframework.web.util.UriComponentsBuilder
 import pl.dayfit.auroraauthlib.auth.provider.MicroserviceAuthProvider
 import pl.dayfit.auroraauthlib.auth.token.MicroserviceTokenCandidate
 import java.security.Principal
-import java.util.UUID
+import java.util.*
 
 @Component
 class TrackerHandshakeInterceptor(
@@ -23,14 +22,11 @@ class TrackerHandshakeInterceptor(
         wsHandler: WebSocketHandler,
         attributes: MutableMap<String, Any>
     ): Boolean {
-        val serverHttpRequest = request as? ServletServerHttpRequest ?: return false
-        val accessToken = serverHttpRequest.servletRequest.getHeader(HttpHeaders.AUTHORIZATION)
-            ?.takeIf { it.startsWith("Bearer ") }
-            ?.removePrefix("Bearer ")
-            ?.trim()
-            ?: serverHttpRequest.servletRequest.getParameter("token")
-                ?.trim()
-            ?: throw BadCredentialsException("No access token found")
+        val accessToken = UriComponentsBuilder
+            .fromUri(request.uri)
+            .build()
+            .queryParams
+            .getFirst("token") ?: throw BadCredentialsException("Token is missing")
 
         val auth = microserviceAuthProvider
             .authenticate(
