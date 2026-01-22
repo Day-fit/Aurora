2082e6ba8c722c7a4d4031b373e546dc80590183
Day-fit <xdayfit@gmail.com>
2026-01-22 13:36:09 +0100

    Integrate `RedisTemplate` into `ResumeCacheService` for improved cache handling and implement cache update logic for `resumes.ownerId` on resume deletion. Add `RedisTemplate` bean to `RedisConfiguration`.

--- aurora-core/src/main/kotlin/pl/dayfit/auroracore/configuration/RedisConfiguration.kt
+++ aurora-core/src/main/kotlin/pl/dayfit/auroracore/configuration/RedisConfiguration.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @Configuration
@@ -14,4 +15,7 @@ class RedisConfiguration {
     fun cacheManager(
         redisConnectionFactory: RedisConnectionFactory
     ): CacheManager = RedisCacheManager.create(redisConnectionFactory)
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any> = RedisTemplate()
 }
--- aurora-core/src/main/kotlin/pl/dayfit/auroracore/service/cache/ResumeCacheService.kt
+++ aurora-core/src/main/kotlin/pl/dayfit/auroracore/service/cache/ResumeCacheService.kt
@@ -1,19 +1,23 @@
 package pl.dayfit.auroracore.service.cache
 
+import com.fasterxml.jackson.module.kotlin.convertValue
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.CachePut
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.cache.annotation.Caching
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Service
 import pl.dayfit.auroracore.model.Resume
 import pl.dayfit.auroracore.repository.ResumeRepository
-import java.util.UUID
+import java.util.*
 
 @Service
 class ResumeCacheService(
     private val resumeRepository: ResumeRepository,
-    private val cacheManager: CacheManager
+    private val cacheManager: CacheManager,
+    private val redisTemplate: RedisTemplate<String, Any>
 ) {
     @Cacheable("resume.id", key = "#id")
     fun getResumeById(id: UUID): Resume {
@@ -38,11 +42,25 @@ class ResumeCacheService(
             .save(resume)
     }
 
-    @Suppress("unused")
     fun deleteResume(id: UUID)
     {
+        val resume = resumeRepository.findById(id)
+            .orElseThrow { NoSuchElementException("There is no resume with such a id") }
+
         cacheManager.getCache("resume.id")
-        //TODO: when adding removing functionality, please implement "resumes.ownerId" cache evict
+            ?.evict(id)
+
+        val resumes = jacksonObjectMapper().convertValue<List<Resume>>(
+            redisTemplate.opsForValue()
+                .get("resumes.ownerId:${resume.auroraUserId}") ?: emptyList<Resume>()
+        )
+
+        cacheManager.getCache("resumes.ownerId")?.put(
+            resume.auroraUserId,
+            resumes.filter { it.id != id }
+                .toList()
+        )
+
         resumeRepository
             .deleteById(id)
     }
